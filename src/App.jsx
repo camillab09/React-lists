7c02a923c59045688477ee849363345cfdf1bef4
Camilla <camillab09.kodehode@gmail.com>
2024-02-12 12:53:57 +0100

    moved const to own folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import { pals } from "./PalDeckArray/PalDeckArray.jsx";
+import PalDeck from "./PalDeck/PalDeck.jsx";
 
 import "./App.css";
-
-const PalDeck = () => (
-  <div className="PalDeckDiv">
-    {pals.map((pal) => (
-      <div className="PalContainer" key={pal.number}>
-        <div className="PalTitle">
-          <h2 className="PalNumber">{pal.number}</h2>
-          <h2 className="PalName">{pal.name}</h2>
-        </div>
-        <div className="PalImageContainer">
-          <img src={pal.img} alt={pal.name} className="PalImages" />
-          <div className="PalElementContainer">
-            <img
-              src={pal.elementImg}
-              alt={pal.element}
-              className="PalElementImg"
-            />
-            <p className="PalElement">{pal.element}</p>
-            <img
-              src={pal.elementImg2}
-              alt={pal.element2}
-              className="PalElementImg"
-            />
-            <p className="PalElement">{pal.element2}</p>
-          </div>
-        </div>
-      </div>
-    ))}
-  </div>
-);
 
 function App() {
   return (
