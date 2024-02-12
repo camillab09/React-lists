e2d81ed0d3e9955494804e5ca54f5b2f0ce2bd7e
Camilla <camillab09.kodehode@gmail.com>
2024-02-12 13:11:20 +0100

    Added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,14 @@
-import PalDeck from "./PalDeck/PalDeck.jsx";
+import { PalDeck } from "./PalDeck/PalDeck.jsx";
+import { Layout } from "./Layout/Layout.jsx";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="pal-title">Paldex</h1>
-
-      <div className="mainDiv">
-        <PalDeck />
-      </div>
-      <footer>
-        <p>
-          Palworld is copyright of Pocketpair inc. This page is for
-          entertainment purposes only.
-          <br /> All images sourced from
-          <a href="https://palworld.fandom.com/wiki/Paldeck">Palworld Wiki</a>
-        </p>
-      </footer>
-    </>
+    <Layout>
+      <PalDeck />
+    </Layout>
   );
 }
 
 export default App;
-
-/*funksjon.map((value) =>{
-lager et nytt element for hver verdi i arrayet
-}) */
-
-/**
- * Sette opp flere sider:
- * $ npm install react-router-dom
- * sette opp en mappe for pages
- * pages>HomePage.jsx
- * (export function HomePage(){
- * return
- * (
- * <main>
- * Home
- * </main>
- * )})
- * pages>contact.jsx
- * (export function Contact(){
- * return(
- * <content>
- * Contact
- * </content>
- * )})
- * pages>rootLayout.jsx
- * (import {outlet, Link, NavLink} from "react-router-dom"
- * export function layout(){
- * return(
- *<nav>
- <li><Link to="/">Home</Link></li>
- <li><Link to="/contact">Contact</Link></li></nav>)
- * })
- * app.jsx
- * (import {BrowserRouter, Routes, Route} from 'react-router-dom
- * function App()){
- * return(
- * <BrowserData>
- * <Routes>
- * <Route path='/' element={<HomePage/>}/> ('/) er defaultsiden)
- * <Route path='/contact' element={<Contact/>}/>
- * </Routes>
- * </BrowserData>)}
- */
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import "../App.css";
+
+export function Layout(properties) {
+  return (
+    <div>
+      <h1 className="pal-title">Paldex</h1>
+
+      <div className="mainDiv">{properties.children}</div>
+      <footer>
+        <p>
+          Palworld is copyright of Pocketpair inc. This page is for
+          entertainment purposes only.
+          <br /> All images sourced from
+          <a href="https://palworld.fandom.com/wiki/Paldeck">Palworld Wiki</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
